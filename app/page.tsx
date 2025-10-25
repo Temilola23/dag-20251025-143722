704a2187db8b8b80b72c3658e7e6472e8e5c0e03
Temilola Olowolayemo <133580838+Temilola23@users.noreply.github.com>
2025-10-25 16:37:26 -0500

    Initial commit from v0.dev

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { DAGBuilder } from "@/components/dag-builder"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background">
+      <DAGBuilder />
+    </main>
+  )
+}
